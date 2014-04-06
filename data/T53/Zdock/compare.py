1bf030c1aa6c370ef4653f0fa0dac74f11620cd6
Brett Koonce <koonce@gmail.com>
2014-04-06 15:27:29 -0500

    fix ligand matching parser script

--- data/T53/Zdock/compare.py
+++ data/T53/Zdock/compare.py
@@ -1,18 +1,24 @@
 from Bio.PDB import *
 import sys
 
-p=PDBParser()
-s=p.get_structure('B', 'complex.1.pdb')
-atom_list=Selection.unfold_entities(s,'A')
+parser=PDBParser()
+structure=parser.get_structure('model', 'complex.1.pdb')
 
-ns=NeighborSearch(atom_list)
+model=structure[0]
+chainA = model['A']
+chainB = model['B']
 
-for atom in atom_list:
+atom_list1=Selection.unfold_entities(chainA,'A')
+atom_list2=Selection.unfold_entities(chainB,'A')
+
+ns = NeighborSearch(atom_list2)
+
+for atom in atom_list1:
     center=atom.get_coord()
-    R=10.0
+    R=5.0
 
-    neighbor_l=ns.search(center,R)
-    residue_list=Selection.unfold_entities(neighbor_l,'R')
+    neighbor_l = ns.search(center,R)
+    residue_list = Selection.unfold_entities(neighbor_l,'R')
 
     for r in residue_list:
         print r
